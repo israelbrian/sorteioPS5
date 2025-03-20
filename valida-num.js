0bcb5297cc382a7b6042d654b241e3aec700b7d1
israel <zdesenhos@gmail.com>
2025-03-20 19:13:20 -0300

    Adicionando comentarios e correções

--- valida-num.js
+++ valida-num.js
@@ -2,9 +2,14 @@ let numeroRecebido
 // let tempo = 5
 
 function escolherNumero() {
+    // ARMAZENANDO NUMERO RECEBIDO NO INPUT
     let numeroRecebido = document.getElementById("numero").value
-    numeroRecebido = parseFloat(numeroRecebido)
+    // CONVERTENDO-O DE STRING PARA NUMBER
+    numeroRecebido = Number(numeroRecebido)
 
+    // VALIDAÇÕES: RECEBER APENAS NUMEROS ENTRE 1 E 50
+    // SE - 1 OU + 50 BARRAR 
+    // SE NÃO FOR UM NUMERO OU NAO PREENCHER NADA BARRAR 
     if (numeroRecebido === 0 || numeroRecebido < 0 ) {
         alert('Digite um número maior que 0!')
         return false
@@ -15,6 +20,6 @@ function escolherNumero() {
         alert('Digite um número válido!')
         return false
     }
-
+    // ENCAMINHAR PARA A PROXIMA PAGE
     window.location.href = `sorteio.html?numero=${numeroRecebido}`
 }
